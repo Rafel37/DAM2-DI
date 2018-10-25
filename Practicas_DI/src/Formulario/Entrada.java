bc127807602a6c08624596252931926a21854ee5
Rafel37 <rafelperez37@hotmail.es>
2018-10-25 19:02:58 +0200

    Add files via upload

---
+++ Practicas_DI/src/Formulario/Entrada.java
@@ -0,0 +1,17 @@
+
+package Formulario;
+
+import javax.swing.*;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Formulario c = new Formulario();
+                c.setVisible(true); //HACER VISIBLE LA VENTANA
+            }
+        });
+    }
+}
