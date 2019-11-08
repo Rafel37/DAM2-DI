7d1c0292f189f48e7cde5c6ee5688853dac686ec
Rafel37 <rafelperez37@hotmail.es>
2019-11-08 12:34:14 +0100

    Remove duplicated directory

--- Practicas_DI/src/Practica_Calculadora_Trigonometrica/Figura.java
+++
@@ -1,7 +0,0 @@
-package Practica_Calculadora_Trigonometrica;
-
-public abstract class Figura {
-
-    protected abstract double calcularPerimetro();
-    protected abstract double calcularArea();
-}
--- Practicas_DI/src/inicio/Main.java
+++
@@ -1,4 +0,0 @@
-package inicio;
-
-public class Main {
-}
